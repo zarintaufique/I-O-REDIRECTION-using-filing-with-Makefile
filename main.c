08f03295af3818bb1aa9555ab101369cab520175
zarin <81378664+zarintaufique@users.noreply.github.com>
2022-02-20 13:29:26 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "header.h"
 
+int main (){
+
+   code();
+   
+	return 0;
+}
